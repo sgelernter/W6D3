0b19ca69f097627fabb7c852f589c7dccd57bf36
chayacohen <chayac100@gmail.com>
2021-11-10 16:22:02 -0500

    completed project

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -5,13 +5,31 @@ class UsersController < ApplicationController
     end
 
     def create 
-        user = User.new(params.require(:user).permit(:name, :email))
-        user.save!
-        render json: user
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else 
+            render json: user.errors.full_messages, status: :unprocessable_entity 
+        end 
     end 
 
     def show
-        render json: params 
+        render json: User.find(params[:id])
+    end 
+
+    def update 
+        user = User.find(params[:id])
+        user.update!(user_params)
+        render json: user
+    end 
+
+    def destroy 
+        User.destroy(params[:id])
+        render json: User.all
+    end 
+
+    def user_params 
+        params.require(:user).permit(:name, :email)
     end 
 
 end
