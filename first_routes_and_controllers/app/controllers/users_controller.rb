a737c195799af8358d19957eecde41492961f925
chayacohen <chayac100@gmail.com>
2021-11-10 15:02:35 -0500

    started userscontroller

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
         render plain: "I'm in the index action!"
     end
 
+    def create 
+        render json: params
+    end 
+
+    def show
+        render json: params 
+    end 
+
 end
